f09c2ee76ba63ca153fe8a4c7e40c6bb536a3052
Bence Kiglics <bence.kiglics@gmail.com>
2014-08-16 16:19:44 +0200

    added vader style

--- Gemfile
+++ Gemfile
@@ -4,3 +4,8 @@ gem 'rerun'
 gem 'rack'
 gem 'sinatra'
 gem 'httparty'
+
+# Asset dependencies
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
+gem 'less'
+gem 'therubyracer'
--- app.rb
+++ app.rb
@@ -3,8 +3,16 @@ require 'httparty'
 require 'json'
 require './lib/yaml_config'
 require './lib/jenkins/jenkins'
+require 'sinatra/assetpack'
+require 'less'
 
 class App < Sinatra::Base
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/css', from: 'assets/css'
+  }
+
   def initialize
     super
 
