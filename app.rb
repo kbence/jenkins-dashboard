3867e9a97c6cb6b12bd6b76b996885b83909be53
Bence Kiglics <bence.kiglics@gmail.com>
2014-08-18 21:26:20 +0200

    implemented theme setting + added yoda theme

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ require_relative 'lib/yaml_config'
 require_relative 'lib/jenkins/jenkins'
 require_relative 'lib/cache'
 
+DEFAULT_SKIN = 'vader'
+
 class App < Sinatra::Base
   register Sinatra::AssetPack
 
@@ -20,11 +22,16 @@ class App < Sinatra::Base
 
     @config = YamlConfig.get
     @cache = Cache.new
-    @jenkins = Jenkins::Jenkins.new @cache, @config['jenkins']
+    @jenkins = Jenkins::Jenkins.new @cache, @config['jenkins', {}]
+  end
+
+  def skin
+    @config['appearance.theme', DEFAULT_SKIN]
   end
 
   get '/' do
     erb :index, :locals => {
+        :skin => skin,
         :jobs => @jenkins.jobs
     }
   end
